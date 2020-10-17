86399774afa96ce77a2955c8c1b13104cac60ab3
Nicole <nicolekpacha@gmail.com>
2020-10-17 15:46:22 -0600

    w01d01 homework
    testing another commit

--- unit_1/w01d2/homework/first_js_homework.js
+++ unit_1/w01d2/homework/first_js_homework.js
@@ -1,3 +1,3 @@
 /*What does the acronym DRY stand for? 
 Why should we pay attention to it? 
-What programming tools have we learned to write DRY code? */
+What programming tools have we learned to write DRY code? TEST */
