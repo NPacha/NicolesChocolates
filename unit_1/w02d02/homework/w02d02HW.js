b89c007770fbabe8b0542aa788321cdcfd25a577
Nicole <nicolekpacha@gmail.com>
2020-10-21 20:43:41 -0600

    Get Even answered

--- unit_1/w02d02/homework/w02d02HW.js
+++ unit_1/w02d02/homework/w02d02HW.js
@@ -5,3 +5,11 @@
 for(let i = 1; i <= 20; i++){
     console.log(i);
 }
+    //Commit message recorded on terminal to "Easy Going answered"
+
+//Get Even//
+for (let i = 0; i <= 200; i+= 2){
+    console.log(i);
+}
+    //Commit message recorded on terminal to "Get Even answered"
+    
