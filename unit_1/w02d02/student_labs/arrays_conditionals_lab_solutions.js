81fc69f4b0ab10730af6bf176d13c8e991469d59
Nicole <nicolekpacha@gmail.com>
2020-10-21 20:22:39 -0600

    Lab Work

---
+++ unit_1/w02d02/student_labs/arrays_conditionals_lab_solutions.js
@@ -0,0 +1,53 @@
+// const quotes = ['Reach for the stars..', 'Work harder not smarter', 'When life gives you lemons'];
+
+// const randomThings = [1, 10, "Hello", true]
+// randomThings[0];
+// randomThings[2] = "World";
+//  console.log(randomThings);
+
+//  const ourClass = ['Ada', 'Zoom', 'Github', 'Slack'];
+//  ourClass[2];
+//  ourClass[2] = "Octocat";
+// //  ourClass[4] = "Cloud City";
+  
+//  ourClass.push('Cloud City');
+//  console.log(ourClass);
+
+ const myArray = [5 ,10 ,500, 20];
+
+//  myArray.push('Egon');
+//  myArray.pop();
+// myArray.unshift('Bob Marley');
+// console.log(myArray);
+
+// myArray.shift();
+
+// myArray.reverse();
+// console.log(myArray);
+
+//BIGGIE SMALLS//
+// for (let i = 0; i < myArray.length; i++){
+// if ( myArray[i] < 100 ) {
+//     console.log ("little number");
+// } else if (myArray[i] >= 100){
+//     console.log("big number");
+//     }
+// } ;
+
+//MONKEY MIDDLE
+for (let i = 0; i < myArray.length; i++){
+    if (myArray[i] < 5) {
+        console.log('little number');
+    } else if (myArray[i] > 10) {
+        console.log('big number');
+    } else {
+        console.log('monkey');
+    }
+}
+
+
+
+
+
+
+
