5daf02e7e49872dd629bdc0680a20a7c51674850
Nicole <nicolekpacha@gmail.com>
2020-10-25 16:09:29 -0600

    Palindrome completed

--- unit_1/w02d03/homework/js_functions_scope_problem_solving/solutions.js
+++ unit_1/w02d03/homework/js_functions_scope_problem_solving/solutions.js
@@ -1,3 +1,15 @@
 //Verbal Questions//
 //1. The difference between a parameter and an argument is that a parameter is established during function declaration (an input of sorts that modifies the functions behavior), while an argument is established when you invoke, or call a function (uses specific data values).
 //2. Console.log prints the result of somethign within the function to the console, which most users will not be seeing. Return on the other hand, stops the function, and sends the value of your function stored within the computers memory (not on the console).
+//Palindrome
+
+const checkPalindrome = (string) => {
+    let lowerCase = string.toLowerCase();
+    if (lowerCase.split("").reverse().join("") === lowerCase) {
+        return true;
+    }
+    return false;
+}
+console.log(checkPalindrome("Radar"));
+
+console.log(checkPalindrome("Borscht"));
