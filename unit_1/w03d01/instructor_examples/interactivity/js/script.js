c67fc97e0b701fac628e08636371b22a15894b70
misteroh <madeline@done-digital.com>
2020-10-27 15:21:21 -0700

    updates main repo

---
+++ unit_1/w03d01/instructor_examples/interactivity/js/script.js
@@ -0,0 +1,41 @@
+
+// let milk = true;
+// const cereal = true;
+
+// const checkIngredients = () => {
+//     if (milk && cereal) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// const pourMilk = () => {
+//     milk = false;
+// }
+
+// const pourCereal = () => {
+//     milk = false;
+// }
+
+// const cerealFinished = () => {
+//     console.log("All Done")
+// }
+
+// const eatCereal = () => {
+//     let cerealLeft = 10;
+//     while (cerealLeft > 0) {
+//         console.log('crunch')
+//         cerealLeft--;
+//     }
+//     cerealFinished();
+// }
+
+// const makeCereal = () => {
+//     if (checkIngredients()) {
+//         pourMilk();
+//         pourCereal();
+//         eatCereal();
+//     }
+// }
+
+// makeCereal();
