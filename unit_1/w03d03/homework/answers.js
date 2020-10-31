3e01e46b7d6ac6831f22557c26d8e63b2ae33916
Nicole <nicolekpacha@gmail.com>
2020-10-31 14:58:56 -0600

    Datatype Refresher Complete

---
+++ unit_1/w03d03/homework/answers.js
@@ -0,0 +1,50 @@
+//////DATATYPES REFRESHER/////
+// // 1. A light switch that can be either on or off.
+// => datatype: boolean 
+// => data structure example: const lightOn = true; 
+// // 2. A user's email address.
+// => datatype: string 
+// => data structure example: const emailaddress = 'nicolekpacha@gmail.com'; 
+// // 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// => datatype: object 
+// => data structure example: const spaceship = {
+//     parts: ['hull', 'laser blaseter', 'tractor beam', 'warp drive']
+// }
+// // 4. A list of student names from our class.
+// => datatype: array
+// => data structure example: const studentNames = ['Nicole', 'Sawyer', 'James'] 
+// // 5. A list of student names from our class, each with a location.
+// => datatype: array of objects
+// => data structure example: const students = [
+//     {
+//         name: 'Nicole',
+//         location: 'Denver',
+//     },
+//     {
+//         name: 'Sawyer',
+//         location: 'NY'
+//     },
+//     {
+//         name: 'James',
+//         location: 'Chicago'
+//     }
+// ]
+// // 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// => datatype: array of objects
+// => data structure example: => const students = [
+//     {
+//         name: 'Nicole',
+//         location: 'Denver',
+            // favMovies: ['ameri', 'sfda']
+//     },
+//     {
+//         name: 'Sawyer',
+//         location: 'NY',
+            // favMovies: ['asf','asdf']
+//     },
+//     {
+//         name: 'James',
+//         location: 'Chicago'
+            // favMovies: ['asf','asdf']
+//     }
+// ]
