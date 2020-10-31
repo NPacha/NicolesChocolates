277bf3dba03b622d53949086be3252873d36ac9e
Nicole <nicolekpacha@gmail.com>
2020-10-31 15:24:39 -0600

    objectception complete

--- unit_1/w03d03/homework/answers.js
+++ unit_1/w03d03/homework/answers.js
@@ -110,3 +110,26 @@ const crazyObject = {
   crazyObject.larry.quotes.push('I\'m trying to elevate small talk to medium talk');
   console.log(crazyObject.larry.quotes);
 
+  /////////////Object-ception//////////////
+
+  const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+ inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo= null;
+ console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
+
