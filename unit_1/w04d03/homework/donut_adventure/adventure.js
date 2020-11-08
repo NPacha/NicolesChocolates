ad1303186b432d1c39f956ad0bd95578c005bb45
Nicole <nicolekpacha@gmail.com>
2020-11-08 07:19:19 -0700

    Created our enemy pizza rat

--- unit_1/w04d03/homework/donut_adventure/adventure.js
+++ unit_1/w04d03/homework/donut_adventure/adventure.js
@@ -23,3 +23,29 @@ class Hero {
 
 const newHero = new Hero('Dougie the donut');
 console.log(newHero);
+
+class Enemy {
+    constructor(name, health = 100, weapons, catchPhrases){
+        this.name = name;
+        this.health = health;
+        this.weapons = weapons || {
+            pepperoniStars: 5,
+            cheeseGrease: 10    
+        },
+        this.catchPhrases = catchPhrases || ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer'];
+    }
+    talkSmack(){
+        let randomIndex = Math.floor(Math.random() * this.catchPhrases.length);
+        console.log(this.catchPhrases[randomIndex])
+    }
+    announceHealth(){
+        console.log(`Current Healt: ${this.health}`)
+    }
+    fight(){
+        console.log('i\'m gonna flatten you like a slice of pepperoni!')
+    }
+}
+
+const newEnemy = new Enemy('Pizza Rat');
+console.log(newEnemy);
