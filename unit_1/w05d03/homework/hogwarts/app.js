5898f750071162b3c9b1380f386d699e73932a32
Nicole <nicolekpacha@gmail.com>
2020-11-14 15:35:16 -0700

    Commit 1 - I added my first element and it was fun

--- unit_1/w05d03/homework/hogwarts/app.js
+++ unit_1/w05d03/homework/hogwarts/app.js
@@ -1,3 +1,19 @@
 // if (typeof jQuery == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+$(() => {
+    // put jQuery in here
+    const $container = $('#container');
+    console.log($container)
+
+    const $h1 = $('<h1>');
+    console.log($h1);
+
+   $h1.text('Hogwarts School of Witchcraft and Wizardry');
+   $container.append($h1);
+    //This text will not appear because even though we created the h1 element, we did not append it to anything yet. 
+
+
+
+});
