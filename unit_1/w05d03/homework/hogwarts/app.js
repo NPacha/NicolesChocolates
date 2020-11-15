bac600b874488a6c6f8c96aaa051d72429799a7d
Nicole <nicolekpacha@gmail.com>
2020-11-14 17:02:07 -0700

    Commit 4- I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- unit_1/w05d03/homework/hogwarts/app.js
+++ unit_1/w05d03/homework/hogwarts/app.js
@@ -35,4 +35,45 @@ const $li5 = $('<li>').addClass('dog').text('leash');
 const $li6 = $('<li>').text('Bertie Bott\'s Every Flavor [Jelly] Beans');
 $ul.append($li1, $li2, $li3, $li4, $li5, $li6)
 
+
+//////////YEAR 4///////////
+const $table = $('<table>');
+$container.append($table)
+
+const $h5 = $('<h5>').text('Spring 2017');
+$table.before($h5);
+
+const $thead = $('<thead>');
+$table.append($thead);
+
+const $th1 = $('<th>').text('Day');
+const $th2 = $('<th>').text('Classes');
+$thead.append($th1, $th2);
+
+const $tr1 = $('<tr>');
+const $td1 = $('<td>').text('Monday');
+const $td2 = $('<td>').text('Herbology, Defense Against the Dark Arts');
+$tr1.append($td1, $td2);
+const $tr2 = $('<tr>');
+const $td3 = $('<td>').text('Tuesday');
+const $td4 = $('<td>').text('Divination, Quidditch practice');
+$tr2.append($td3, $td4);
+const $tr3 = $('<tr>');
+const $td5 = $('<td>').text('Wednesday');
+const $td6 = $('<td>').text('History of Magic');
+$tr3.append($td5, $td6);
+const $tr4 = $('<tr>');
+const $td7 = $('<td>').text('Thursday');
+const $td8 = $('<td>').text('Charms');
+$tr4.append($td7, $td8);
+const $tr5 = $('<tr>');
+const $td9 = $('<td>').text('Friday');
+const $td10 = $('<td>').text('Potions, Transfiguration');
+$tr5.append($td9, $td10);
+
+$table.append($tr1, $tr2, $tr3, $tr4, $tr5)
+
+
+
+
 });
