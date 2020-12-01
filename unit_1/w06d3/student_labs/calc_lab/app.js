93418361c9f2356d55f850e73b55d3b4bbef9280
Nicole <nicolekpacha@gmail.com>
2020-12-01 15:30:13 -0700

    Before class

--- unit_1/w06d3/student_labs/calc_lab/app.js
+++ unit_1/w06d3/student_labs/calc_lab/app.js
@@ -1,5 +1,31 @@
 $(() => {
-  
-  
+  //Cache DOM Nodes
+  const $number = $('.num');
+  const $operator = $('.operator');
+  const $button = $('button');
+  const $output = $('.output');
+  const number = [];
+
+//Event Handler
+const doOperation = () => {
+    
+
+}
+
+
+const displayNumber = (event) =>{
+    const $target = $(event.target);
+    console.log($target.html());
+    number.push($target.html());
+    number.join('');
+    console.log(number);
+    $output.html(number);
+    $operator.on('click', doOperation)
+}
+
+
+  //Event Listner
+  $button.on('click', displayNumber)
   
 });
+
