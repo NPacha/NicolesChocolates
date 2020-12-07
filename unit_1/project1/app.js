fe6e0638641d6ee91647e48eabcbd83734cf4540
Nicole <nicolekpacha@gmail.com>
2020-12-07 10:14:04 -0700

    logic for appending words created

--- unit_1/project1/app.js
+++ unit_1/project1/app.js
@@ -2,8 +2,11 @@
 //Create an Alphabet Array
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 
+//Cache DOM nodes
 const $letters = $('.letters');
-console.log($letters);
+const $wordBox = $('.words');
+const $input = $('input');
+const $addButton = $('#submit');
 
 //Create a player class
 class Player {
@@ -36,11 +39,10 @@ class LettersBox {
 }
 const lettersBox = new LettersBox;
 
-//Generate 7 random numbers
-lettersBox.generateLetters();
-console.log(lettersBox)
 
-//Create a letters box class
+//Create a word box array to store the words
+const wordsBox = [];
+
 
 //Create a score board object
 const scoreBoard = {
@@ -48,6 +50,30 @@ const scoreBoard = {
     player1Points: 0,
     player2Points: 0
 }
+
+//Generate 7 random numbers
+lettersBox.generateLetters();
+console.log(lettersBox)
+
+//Event Handlers
+const addWord = () => {
+    // event.stopPropogation();
+    const $li = $('<li>').html($input.val());
+    const $disputeButton = $('<button>').addClass('dispute').text('DISPUTE');
+    const $deleteButton = $('<button>').addClass('dispute').text('DELETE');
+    $li.append($disputeButton);
+    $li.append($deleteButton);
+    // $disputeButton.on('click', displayDictionary);
+    // $deleteButton.on('click', deleteWord)
+    $wordBox.append($li);
+    $input.val('');
+
+}
+
+//Add event listener for ADD button
+$addButton.on('click', ()=>{addWord()})
+
+
 
 
 
